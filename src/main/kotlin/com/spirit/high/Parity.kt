d2cc99c9fbe109c29e24f02e27fdeb86b2025972
Shilpan Patel <shilpanp@yahoo-inc.com>
2017-10-23 22:11:22 -0700

    Update gradle

---
+++ src/main/kotlin/com/spirit/high/Parity.kt
@@ -0,0 +1,17 @@
+package com.spirit.high
+
+class Parity {
+
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+
+        }
+    }
+
+    fun parityOne(x: Long) : Short {
+
+
+        return 0
+    }
+
+}
